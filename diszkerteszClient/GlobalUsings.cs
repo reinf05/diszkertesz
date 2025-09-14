b5c3cc76d748257a55401d8d1f2191afdd3899fd
EgyetemPC <Egyetem@DESKTOP-8S29QAQ>
2025-09-14 18:10:42 +0200

    a

--- diszkerteszClient/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-global using Díszkertész_kliens.Data;
-global using Díszkertész_kliens.PageModels;
-global using Díszkertész_kliens.Pages;
-global using Díszkertész_kliens.Services;
-global using Díszkertész_kliens.Utilities;
-global using Fonts;
--- diszkerteszClient/Models/ProjectsTags.cs
+++
@@ -1,9 +0,0 @@
-namespace Díszkertész_kliens.Models
-{
-    public class ProjectsTags
-    {
-        public int ID { get; set; }
-        public int ProjectID { get; set; }
-        public int TagID { get; set; }
-    }
-}
